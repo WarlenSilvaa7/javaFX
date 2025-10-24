a34e506797b1b7ac230c148049b7382d2ca4a927
Warlen <warlensa007@gmail.com>
2025-10-24 14:46:43 -0300

    feat(learning): Add sum calculator (TextField/Label)

--- src/main/java/Controllers/ViewController.java
+++ src/main/java/Controllers/ViewController.java
@@ -3,14 +3,36 @@ package Controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import util.Alerts;
+
+import java.util.Locale;
 
 public class ViewController {
 
     @FXML
-    private Button btTest;
+    private TextField txtNumber1;
+    @FXML
+    private TextField txtNumber2;
 
-    public void onBtTestAction(){
-    Alerts.showAlert("Alert title", "Alert header", "Hello", Alert.AlertType.INFORMATION);
+    @FXML
+    private Label labelResult;
+
+    @FXML
+    private Button btSum;
+
+    public void onBtSumAction(){
+        try{
+            Locale.setDefault(Locale.US);
+            double number1 = Double.parseDouble(txtNumber1.getText());
+            double number2 = Double.parseDouble(txtNumber2.getText());
+            double sum = number1 + number2;
+            labelResult.setText(String.format("%.2f", sum));
+        }
+        catch (NumberFormatException e){
+            Alerts.showAlert("Error", "Parse error", e.getMessage(), Alert.AlertType.ERROR);
+
+        }
     }
 }
